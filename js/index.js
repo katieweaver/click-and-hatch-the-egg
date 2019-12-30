41f2eebebbd4ed4472e968f28f17c3a25b2e03b1
Katie Weaver <34561729+katieweaver@users.noreply.github.com>
2019-12-29 23:41:40 -0600

    some small changes

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let tempTrainerName = 'Kaive Cordath';
+let tempTrainerId = '0000-0000-0000'
+
+document.getElementById('td-trainer-name').innerHTML = tempTrainerName;
+document.getElementById('td-trainer-id').innerHTML = tempTrainerId;
